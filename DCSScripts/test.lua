252cdf6962c2072e36c35f0d6b6af36fac84c9f4
Igneous01 <5571589+Igneous01@users.noreply.github.com>
2017-12-16 21:12:34 -0500

    New Utility - SlingloadEventsWatcher
    Created new C# Solution utility that watches DCS log file for slingload events and writes them to a file. These will later be picked up in mission to create slingload game events.

--- DCSScripts/test.lua
+++ DCSScripts/test.lua
@@ -1,4 +1,45 @@
+if not KI then
+  KI = {}
+end
+
+if not KI.Toolbox  then
+  KI.Toolbox = {}
+end
+
+
+function KI.Toolbox.Dump(o)
+  if type(o) == 'table' then
+    local s = '{ '
+    for k,v in pairs(o) do  
+      if k == "__index" then 
+        s = s .. '"' .. k .. '" = "table"'
+      else
+        if type(k) ~= 'number' then k = '"'..k..'"' end
+        s = s .. '['..k..'] = ' .. KI.Toolbox.Dump(v) .. ','
+      end
+    end
+    return s .. '} '
+  else
+    return tostring(o)
+  end
+end
+
+local wood = StaticObject.getByName("cargo_wood")
+local uh1h = StaticObject.getByName("cargo_uh1h")
+local cont = StaticObject.getByName("cargo_cont")
+local pipe = StaticObject.getByName("cargo_pipe")
+
+env.info("wood _id: " .. tostring(wood.id_))
+env.info("uh1h _id: " .. tostring(uh1h.id_))
+env.info("cont _id: " .. tostring(cont.id_))
+env.info("pipe _id: " .. tostring(pipe.id_))
+
+env.info("wood desc: " .. KI.Toolbox.Dump(wood))
+env.info("uh1h desc: " .. KI.Toolbox.Dump(uh1h))
+env.info("cont desc: " .. KI.Toolbox.Dump(cont))
+env.info("pipe desc: " .. KI.Toolbox.Dump(pipe))
+
 --AICOM.DoTurn({}, 0)
 
 --KI.Loader.SaveData()
-KI.Loader.LoadData()
+--KI.Loader.LoadData()
---
+++ Utilities/SlingloadEventsWatcher/SlingloadEventsWatcher/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Threading;
+
+namespace SlingloadEventsWatcher
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Invalid Arguments Provided - expected 2 arguments : [1] DCS.log directory path [2] Slingload events write directory");
+                Console.ReadKey();
+                return;
+            }
+            else if (!Directory.Exists(args[0]))
+            {
+                Console.WriteLine("Invalid Arguments Provided - path '" + args[0] + "' cannot be found");
+                Console.ReadKey();
+                return;
+            }
+            else if (!Directory.Exists(args[1]))
+            {
+                Console.WriteLine("Invalid Arguments Provided - path '" + args[1] + "' cannot be found");
+                Console.ReadKey();
+                return;
+            }
+
+            string path = args[0];
+            string writePath = args[1];
+            string ChoosingCargoString = "DCS: CargoManager::choosingCargo:";
+            string HookCargoString = "DCS: chooseCargo:";
+            string UnhookCargoString = "DCS: unhookCargo:";
+            string CargoCrashString = "DCS: cargoCrashed:";
+
+            AutoResetEvent wh = new AutoResetEvent(false);
+            FileSystemWatcher fsw = new FileSystemWatcher(args[0])
+            {
+                Filter = "dcs.log",
+                EnableRaisingEvents = true
+            };
+            fsw.Changed += (s, e) => wh.Set();
+
+            FileStream fs = new FileStream(path + "\\dcs.log", FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            using (var sr = new StreamReader(fs))
+            {
+                string s = "";
+                Dictionary<string, string> hash = new Dictionary<string, string>();
+                int filecount = 0;
+                while (true)
+                {
+                    while(true)
+                    {
+                        try
+                        {              
+                            s = sr.ReadLine();
+                            if (s == null)
+                                break;
+
+                            if (s.Contains(ChoosingCargoString))
+                            {
+                                int startIndex = s.IndexOf(ChoosingCargoString) + ChoosingCargoString.Length;
+                                int lastIndex = s.IndexOf(' ', startIndex) - startIndex;
+
+                                string cargoID = s.Substring(startIndex, lastIndex);
+                                string heliID = "";
+
+                                if (s.Contains("HelId:"))
+                                {
+                                    int sIndex = s.IndexOf("HelId:") + "HeliID".Length;
+                                    int lIndex = s.Length - sIndex;
+                                    heliID = s.Substring(sIndex, lIndex);
+                                }
+                                Console.WriteLine("DCS: Player Choosing Cargo (heliID: " + heliID + ", cargoID: " + cargoID + ")");
+                                hash[heliID] = cargoID;
+                            }                       
+                            else if (s.Contains(HookCargoString))
+                            {
+                                int startIndex = s.IndexOf(HookCargoString) + HookCargoString.Length;
+                                int lastIndex = s.Length - startIndex;
+                                string heliID = s.Substring(startIndex, lastIndex);
+                                double time = double.Parse(s.Substring(0, 9), NumberStyles.Any);
+                                if (hash.ContainsKey(heliID))
+                                {
+                                    Console.WriteLine("DCS: Player Hooked Cargo (heliID: " + heliID + ", cargoID: " + hash[heliID] + ")");
+                                    string luastring = "local t = {\n\t[\"Event\"] = \"HOOK\", [\"HeliID\"] = \"" + heliID + "\", [\"CargoID\"] = \"" + hash[heliID] + "\", [\"Time\"] = " + time + "\n}";
+                                    File.WriteAllText(writePath + "\\sl_eh_" + filecount + ".lua", luastring);
+                                    filecount++;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("WARNING - Could not find heliID in hash (heliID: " + heliID + ")");
+                                }
+                            }
+                            else if (s.Contains(UnhookCargoString))
+                            {
+                                int startIndex = s.IndexOf(UnhookCargoString) + UnhookCargoString.Length;
+                                int lastIndex = s.Length - startIndex;
+                                string heliID = s.Substring(startIndex, lastIndex);
+                                double time = double.Parse(s.Substring(0, 9), NumberStyles.Any);
+                                if (hash.ContainsKey(heliID))
+                                {
+                                    Console.WriteLine("DCS: Player Unhooked Cargo (heliID: " + heliID + ", cargoID: " + hash[heliID] + ")");
+                                    string luastring = "local t = {\n\t[\"Event\"] = \"UNHOOK\", [\"HeliID\"] = \"" + heliID + "\", [\"CargoID\"] = \"" + hash[heliID] + "\", [\"Time\"] = " + time + "\n}";
+                                    File.WriteAllText(writePath + "\\sl_eh_" + filecount + ".lua", luastring);
+                                    filecount++;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("WARNING - Could not find heliID in hash (heliID: " + heliID + ")");
+                                }
+                            }
+                            else if (s.Contains(CargoCrashString))
+                            {
+                                int startIndex = s.IndexOf(CargoCrashString) + CargoCrashString.Length;
+                                int lastIndex = s.Length - startIndex;
+                                string cargoID = s.Substring(startIndex, lastIndex);
+                                double time = double.Parse(s.Substring(0, 9), NumberStyles.Any);
+                                if (hash.ContainsValue(cargoID))
+                                {
+                                    string heliID = hash.FirstOrDefault(x => x.Value == cargoID).Key;
+                                    Console.WriteLine("DCS: Player Destroyed Cargo (heliID: " + heliID + ", cargoID: " + cargoID + ")");
+                                    string luastring = "local t = {\n\t[\"Event\"] = \"UNHOOK_CRASH\", [\"HeliID\"] = \"" + heliID + "\", [\"CargoID\"] = \"" + cargoID + "\", [\"Time\"] = " + time + "\n}";
+                                    File.WriteAllText(writePath + "\\sl_eh_" + filecount + ".lua", luastring);
+                                    filecount++;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("WARNING - Could not find heliID in hash (cargoID: " + cargoID + ")");
+                                }
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine("Error encountered: " + ex.Message);
+                            break;
+                        }
+                    }
+                    
+                    wh.WaitOne(1000);
+                }
+            }
+
+            wh.Close();
+        }
+    }
+}
