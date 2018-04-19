2044d4be4ed31a98d506d0ec987ec3b2748334ba
Igneous01 <5571589+Igneous01@users.noreply.github.com>
2018-04-18 23:00:55 -0400

    Removed WebApp, SQL, TCPServer
    Removed redundant solutions from repository

--- SQL/Jobs/ki_job_add_player_session_series.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO rpt_player_session_series (ucid, session_id, event, game_time)
-SELECT g.ucid, g.session_id, g.event, g.model_time 
-FROM tmp_gameevents g
-WHERE g.ucid IS NOT NULL AND 
-	  g.session_id IS NOT NULL AND 
-      g.event IS NOT NULL AND 
-      g.model_time IS NOT NULL AND
-      g.ucid IS NOT NULL;
-      
